135bce3a50025c3eeb65244f85db29aac393a477
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-06-17 10:11:55 +0800

    counter task finish

---
+++ src/components/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <button @click="increment()">+</button>
+    <span class="count_diplay">{{ count }}</span>
+    <button @click="descrement()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+    descrement() {
+      this.count -= 1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+
+.count_diplay {
+  margin-left: 16px;
+  margin-right: 16px;
+}
+</style>
